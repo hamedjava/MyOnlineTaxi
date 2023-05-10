8ec36dff1c35d00ddbd0dd4c41543613122f0b4a
hamedjava <hamedpouyanfar1987@gmail.com>
2023-05-10 21:13:54 +0430

    latest commit

---
+++ online_taxi/lib/model/globals/globals.dart
@@ -0,0 +1,5 @@
+import 'package:location_picker_flutter_map/location_picker_flutter_map.dart';
+
+class Globals {
+  static List<LatLong> location = <LatLong>[];
+}
--- online_taxi/lib/view/screens_pages/home/index.dart
+++ online_taxi/lib/view/screens_pages/home/index.dart
@@ -12,6 +12,7 @@ class Index extends StatefulWidget {
 
 class _IndexState extends State<Index> {
   TextEditingController search = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,51 +27,84 @@ class _IndexState extends State<Index> {
         ),
       ),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width / 1.3,
-              height: 500,
-              color: Colors.transparent,
-              child: FlutterLocationPicker(
-                  initPosition: LatLong(23, 89),
-                  selectLocationButtonStyle: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue),
-                  ),
-                  selectLocationButtonText: 'Set Current Location',
-                  initZoom: 11,
-                  showSearchBar: true,
-                  showCurrentLocationPointer: true,
-                  minZoomLevel: 5,
-                  maxZoomLevel: 16,
-                  trackMyPosition: true,
-                  onError: (e) => print(e),
-                  onPicked: (pickedData) {
-                    pickedData.latLong.latitude;
-                    pickedData.latLong.longitude;
-                    pickedData.address;
-                    pickedData.addressData['country'];
-                  }),
-            )
-          ],
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 1000,
+                color: Colors.transparent,
+                child: FlutterLocationPicker(
+                    initPosition: LatLong(23, 89),
+                    selectLocationButtonStyle: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue),
+                    ),
+                    selectLocationButtonText: 'Set Current Location',
+                    initZoom: 11,
+                    showSearchBar: true,
+                    showCurrentLocationPointer: true,
+                    minZoomLevel: 5,
+                    maxZoomLevel: 16,
+                    trackMyPosition: true,
+                    onPicked: (pickedData) {
+                      pickedData.latLong.latitude;
+                      pickedData.latLong.longitude;
+                      pickedData.address;
+                      pickedData.addressData['country'];
+                    }),
+              )
+            ],
+          ),
         ),
       ),
-      bottomSheet: Container(
-        color: Colors.white,
-        width: double.infinity,
-        height: 180,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomTextField(
-                title: "جستجو", controller: search, validator: (_) {}),
-            CustomButton(title: "انتخاب مبدأ", onPressed: () {}),
-          ],
+      bottomSheet: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            color: Colors.white,
+            width: double.infinity,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomTextField(
+                      title: "جستجو",
+                      controller: search,
+                      validator: (_) {
+                        if (_formKey.currentState!.validate()) {
+                          return "";
+                        } else {
+                          return "Error";
+                        }
+                      }),
+                  CustomButton(title: "انتخاب مبدأ", onPressed: () {}),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
+
+  List<LatLong> parseLatLngFromStringList(List<String> strings) {
+    List<LatLong> latLngs = [];
+    for (String s in strings) {
+      List<String> parts = s.split(',');
+      double lat = double.parse(parts[0]);
+      double lng = double.parse(parts[1]);
+      latLngs.add(LatLong(lat, lng));
+    }
+    return latLngs;
+  }
+
+  // moveToSearchedLocationUser(String location) {
+  //   parseLatLngFromStringList();
+  // }
 }
